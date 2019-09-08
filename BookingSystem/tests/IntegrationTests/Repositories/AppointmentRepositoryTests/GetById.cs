81ddc75fdb883420edad9ca50c06c1b19a5c59fa
Julia <juliawaloszek@gmail.com>
2019-09-08 20:37:36 +0200

    Add IntegrationTests

---
+++ BookingSystem/tests/IntegrationTests/Repositories/AppointmentRepositoryTests/GetById.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookingSystem.Infrastructure.Data;
+
+namespace IntegrationTests.Repositories.AppointmentRepositoryTests
+{
+    public class GetById
+    {
+        private readonly BookingSystemContext _context;
+
+        public GetById(BookingSystemContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
